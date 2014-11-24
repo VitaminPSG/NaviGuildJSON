cae1c7cbad15b058edc474905c92be0fb41bf53f
Sergey <bawl@list.ru>
2014-11-24 16:26:32 +0300

    Константы

---
+++ Constants.java
@@ -0,0 +1,10 @@
+public class Constants {
+    // Константы типов транспорта
+    public static final int MOVE = 0;
+    public static final int BUS = 1;
+    public static final int TROL = 2;
+    public static final int TRAM = 4;
+    public static final int METRO = 8;
+    public static final int TAXI = 16;
+    public static final int RAILWAY = 32;
+}
